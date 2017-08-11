ec811987d689e6da885d11666065bbd61eacb496
Lino Jimenez <linojr+linocodes@gmail.com>
2017-08-10 22:07:55 -0500

    Wired up API request to fcc top brownie points

--- src/App.js
+++ src/App.js
@@ -19,3 +19,35 @@ class App extends Component {
 }
 
 export default App;
+
+//code used to call API of the top 100 fccampers who had
+//the most brownie points in the last 30 days.
+function takeRes(res){
+  console.log("we made it into takeRes")
+  return res;
+}
+
+const httpRequest = new XMLHttpRequest();
+(
+function makeRequest() {
+  if (!httpRequest) {
+    alert('Giving up :( Cannot create an XMLHTTP instance');
+    return false;
+  }
+  httpRequest.onreadystatechange = alertContents;
+  httpRequest.open('GET', 'https://fcctop100.herokuapp.com/api/fccusers/top/recent');
+  httpRequest.send();
+}
+)();
+
+function alertContents() {
+    if (httpRequest.readyState === XMLHttpRequest.DONE) {
+      if (httpRequest.status === 200) {
+        console.log("we made it");
+        takeRes(httpRequest.responseText);
+        //httpRequest.responseText
+      } else {
+        console.log('There was a problem with the request.');
+      }
+    }
+  }
